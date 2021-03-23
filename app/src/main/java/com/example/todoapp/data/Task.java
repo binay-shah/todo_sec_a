091933615017050944194236abc19cab92d9bd6f
binay shah <binayshah@binays-MacBook-Pro.local>
2021-03-23 07:13:38 +0545

    database implemented

--- app/src/main/java/com/example/todoapp/data/Task.java
+++ app/src/main/java/com/example/todoapp/data/Task.java
@@ -1,4 +1,4 @@
-package com.example.todoapp;
+package com.example.todoapp.data;
 
 import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
--- app/src/main/java/com/example/todoapp/data/TodoDao.java
+++ app/src/main/java/com/example/todoapp/data/TodoDao.java
@@ -1,4 +1,4 @@
-package com.example.todoapp;
+package com.example.todoapp.data;
 
 import androidx.room.Dao;
 import androidx.room.Delete;
@@ -6,8 +6,6 @@ import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 import androidx.room.Update;
-
-import com.google.android.material.circularreveal.CircularRevealHelper;
 
 import java.util.List;
 
